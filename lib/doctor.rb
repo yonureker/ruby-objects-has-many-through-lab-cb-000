8ea689089ea84cab3530604d49de181696a109f8
Onur Eker <yonureker@gmail.com>
2018-02-02 01:52:44 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+def Doctor
+  attr_accessor :name
+  attr_reader :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment
+  end
+
+  def patients
+  end
+end
